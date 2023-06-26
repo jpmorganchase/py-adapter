28256bbaa2bbe63257c79ff2da23b69a43b8f1df
faph <faph@users.noreply.github.com>
2023-06-26 12:49:46 +0100

    Improve variable naming

--- src/py_adapter/_schema.py
+++ src/py_adapter/_schema.py
@@ -25,8 +25,8 @@ from typing import Any
 import avro.schema
 from avro import constants
 
-_DEBUG_VALIDATE_INDENT = 0
 _DEBUG_VALIDATE = False
+_debug_validate_indent = 0
 
 INT_MIN_VALUE = -(1 << 31)
 INT_MAX_VALUE = (1 << 31) - 1
@@ -48,8 +48,8 @@ def match(expected_schema: avro.schema.Schema, datum: Any) -> float:
     Values between 0 and 1 are used for record schemas where certain fields might be omitted from the data or where
     extra fields are provided.
     """
-    global _DEBUG_VALIDATE_INDENT
     global _DEBUG_VALIDATE
+    global _debug_validate_indent
     expected_type = expected_schema.type
     name = getattr(expected_schema, "name", "")
     if name:
@@ -58,91 +58,92 @@ def match(expected_schema: avro.schema.Schema, datum: Any) -> float:
         if _DEBUG_VALIDATE:
             print(
                 "{!s}{!s}{!s}: {!s} {{".format(
-                    " " * _DEBUG_VALIDATE_INDENT, expected_schema.type, name, type(datum).__name__
+                    " " * _debug_validate_indent, expected_schema.type, name, type(datum).__name__
                 ),
                 file=sys.stderr,
             )
-            _DEBUG_VALIDATE_INDENT += 2
+            _debug_validate_indent += 2
             if datum is not None and not datum:
-                print("{!s}<Empty>".format(" " * _DEBUG_VALIDATE_INDENT), file=sys.stderr)
+                print("{!s}<Empty>".format(" " * _debug_validate_indent), file=sys.stderr)
         result = _match[expected_type](expected_schema, datum)
         if _DEBUG_VALIDATE:
-            _DEBUG_VALIDATE_INDENT -= 2
-            print("{!s}}} -> {!s}".format(" " * _DEBUG_VALIDATE_INDENT, result), file=sys.stderr)
+            _debug_validate_indent -= 2
+            print("{!s}}} -> {!s}".format(" " * _debug_validate_indent, result), file=sys.stderr)
     else:
         result = _match[expected_type](expected_schema, datum)
         if _DEBUG_VALIDATE:
             print(
                 "{!s}{!s}{!s}: {!s} -> {!s}".format(
-                    " " * _DEBUG_VALIDATE_INDENT, expected_schema.type, name, type(datum).__name__, result
+                    " " * _debug_validate_indent, expected_schema.type, name, type(datum).__name__, result
                 ),
                 file=sys.stderr,
             )
     return result
 
 
-def _is_timezone_aware_datetime(dt) -> bool:
+def _is_timezone_aware_datetime(dt: datetime.datetime) -> bool:
     """Return whether a given datetime object contains a timezone"""
     return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
 
 
-def _match_record(s: avro.schema.RecordSchema, d: Any) -> float:
+def _match_record(schema: avro.schema.RecordSchema, datum: Any) -> float:
     """
     Return the degree to which given data matches a schema
 
     This return a number between 0 (no match/incompatible data) and 1 (all fields are provided). Partial matches simply
     mean some fields are provided.
     """
-    if not isinstance(d, dict):
+    if not isinstance(datum, dict):
         return 0.0
 
     # TODO: the original validation function also recursively validated the data for each field. Need to decide whether
     # that is necessary here or not.
-    provided_fields = d.keys()
-    schema_fields = {f.name for f in s.fields}
+    provided_fields = datum.keys()
+    schema_fields = {f.name for f in schema.fields}
     # This is a rather arbitrary scheme for determining "best" match. It does not consider optional vs required fields.
     # It needs to penalize extra fields certainly.
     return len(provided_fields & schema_fields) / len(provided_fields | schema_fields)
 
 
 _match = {
-    "null": lambda s, d: float(d is None),
-    "boolean": lambda s, d: float(isinstance(d, bool)),
-    "string": lambda s, d: float(
-        isinstance(d, str) or (isinstance(d, (dict, list)) and s.get_prop("logicalType") == "json")
+    "null": lambda schema, datum: float(datum is None),
+    "boolean": lambda schema, datum: float(isinstance(datum, bool)),
+    "string": lambda schema, datum: float(
+        isinstance(datum, str) or (isinstance(datum, (dict, list)) and schema.get_prop("logicalType") == "json")
     ),
-    "bytes": lambda s, d: float(
-        (isinstance(d, bytes)) or (isinstance(d, Decimal) and getattr(s, "logical_type", None) == constants.DECIMAL)
+    "bytes": lambda schema, datum: float(
+        (isinstance(datum, bytes))
+        or (isinstance(datum, Decimal) and getattr(schema, "logical_type", None) == constants.DECIMAL)
     ),
-    "int": lambda s, d: float(
-        ((isinstance(d, int)))
+    "int": lambda schema, datum: float(
+        ((isinstance(datum, int)))
         # Modified here as we serialize dates as timestamps in milliseconds
-        and (LONG_MIN_VALUE <= d <= LONG_MAX_VALUE)
-        or (isinstance(d, datetime.date) and getattr(s, "logical_type", None) == constants.DATE)
-        or (isinstance(d, datetime.time) and getattr(s, "logical_type", None) == constants.TIME_MILLIS)
+        and (LONG_MIN_VALUE <= datum <= LONG_MAX_VALUE)
+        or (isinstance(datum, datetime.date) and getattr(schema, "logical_type", None) == constants.DATE)
+        or (isinstance(datum, datetime.time) and getattr(schema, "logical_type", None) == constants.TIME_MILLIS)
     ),
-    "long": lambda s, d: float(
-        (isinstance(d, int))
-        and (LONG_MIN_VALUE <= d <= LONG_MAX_VALUE)
-        or (isinstance(d, datetime.time) and getattr(s, "logical_type", None) == constants.TIME_MICROS)
+    "long": lambda schema, datum: float(
+        (isinstance(datum, int))
+        and (LONG_MIN_VALUE <= datum <= LONG_MAX_VALUE)
+        or (isinstance(datum, datetime.time) and getattr(schema, "logical_type", None) == constants.TIME_MICROS)
         or (
-            (isinstance(d, datetime.date) and _is_timezone_aware_datetime(d) or isinstance(d, str))
-            and getattr(s, "logical_type", None) in (constants.TIMESTAMP_MILLIS, constants.TIMESTAMP_MICROS)
+            (isinstance(datum, datetime.date) and _is_timezone_aware_datetime(datum) or isinstance(datum, str))
+            and getattr(schema, "logical_type", None) in (constants.TIMESTAMP_MILLIS, constants.TIMESTAMP_MICROS)
         )
     ),
-    "float": lambda s, d: float(isinstance(d, (int, float))),
-    "fixed": lambda s, d: float(
-        (isinstance(d, bytes) and len(d) == s.size)
-        or (isinstance(d, Decimal) and getattr(s, "logical_type", None) == constants.DECIMAL)
+    "float": lambda schema, datum: float(isinstance(datum, (int, float))),
+    "fixed": lambda schema, datum: float(
+        (isinstance(datum, bytes) and len(datum) == schema.size)
+        or (isinstance(datum, Decimal) and getattr(schema, "logical_type", None) == constants.DECIMAL)
     ),
-    "enum": lambda s, d: float(d in s.symbols),
-    "array": lambda s, d: float(isinstance(d, list) and all(match(s.items, item) for item in d)),
-    "map": lambda s, d: float(
-        isinstance(d, dict)
-        and all(isinstance(key, str) for key in d)
-        and all(match(s.values, value) for value in d.values())
+    "enum": lambda schema, datum: float(datum in schema.symbols),
+    "array": lambda schema, datum: float(isinstance(datum, list) and all(match(schema.items, item) for item in datum)),
+    "map": lambda schema, datum: float(
+        isinstance(datum, dict)
+        and all(isinstance(key, str) for key in datum)
+        and all(match(schema.values, value) for value in datum.values())
     ),
-    "union": lambda s, d: float(any(match(branch, d) for branch in s.schemas)),
+    "union": lambda schema, datum: float(any(match(branch, datum) for branch in schema.schemas)),
     "record": _match_record,
 }
 _match["double"] = _match["float"]
