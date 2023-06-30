6d1231076ef2b8bfa442f766b0fc2fdf151dd15d
faph <faph@users.noreply.github.com>
2023-06-30 10:13:14 +0100

    Add test cases designing the serialization/deserialization API

---
+++ tests/test_serialize.py
@@ -0,0 +1,89 @@
+import io
+
+import pytest
+
+import py_adapter
+
+
+def tsst_invalid_format(ship_obj):
+    with pytest.raises(..., match="'does not exist' serialization format not supported"):
+        py_adapter.serialize(ship_obj, "does not exist")
+
+
+def test_serialize_json(ship_obj, ship_class):
+    data = py_adapter.serialize(ship_obj, format="JSON")
+    expected_serialization = b"""{
+        "cargo": {
+            "description": "Barrels of rum",
+            "weight_kg": 5100.5
+        },
+        "crew": [
+            {"name": "Florenz"},
+            {"name": "Cara"}
+        ],
+        "departed_at": "2022-10-28T13:30:00.000Z",
+        "build_on": "1970-12-31",
+        "engine": null,
+        "id": "00000000-0000-0000-0000-000000000001",
+        "name": "Elvira",
+        "type": "SAILING_VESSEL",
+        "sails": {"main":{"type":"dacron"},"jib":"white"},
+        "tags": ["keelboat"]
+    }""".replace(
+        b" ", b""
+    )
+    assert data == expected_serialization
+    obj_out = py_adapter.deserialize(data, ship_class, format="JSON")  # possibly auto-detect format
+    assert obj_out == ship_obj
+
+
+def test_serialize_avro(ship_obj, ship_class):
+    data = py_adapter.serialize(ship_obj, "Avro")
+    obj_out = py_adapter.deserialize(data, "Avro", ship_class)
+    assert obj_out == ship_obj
+
+
+def test_serialize_stream_json(ship_obj, ship_class):
+    data = io.BytesIO()
+    py_adapter.serialize_to_stream(ship_obj, data, format="JSON")
+    data.seek(0)
+    obj_out = py_adapter.deserialize_from_stream(data, ship_class, format="JSON")
+    assert obj_out == ship_obj
+
+
+def test_serialize_stream_avro(ship_obj, ship_class):
+    data = io.BytesIO()
+    py_adapter.serialize_to_stream(ship_obj, data, format="Avro")
+    data.seek(0)
+    obj_out = py_adapter.deserialize_from_stream(data, ship_class, format="Avro")
+    assert obj_out == ship_obj
+
+
+def test_serialize_many_json(ship_obj, ship_class):
+    ship_objs = [ship_obj, ship_obj]
+    data = py_adapter.serialize_many(ship_objs, format="JSON")
+    objs_out = list(py_adapter.deserialize_many(data, ship_class, format="JSON"))
+    assert objs_out == ship_objs
+
+
+def test_serialize_many_avro(ship_obj, ship_class):
+    ship_objs = [ship_obj, ship_obj]
+    data = py_adapter.serialize_many(ship_objs, format="Avro")
+    objs_out = list(py_adapter.deserialize_many(data, ship_class, format="Avro"))
+    assert objs_out == ship_objs
+
+
+def test_serialize_many_stream_json(ship_obj, ship_class):
+    ship_objs = [ship_obj, ship_obj]
+    data = io.BytesIO()
+    py_adapter.serialize_many_to_stream(ship_objs, data, format="JSON")
+    objs_out = list(py_adapter.deserialize_many_from_stream(data, ship_class, format="JSON"))
+    assert objs_out == ship_objs
+
+
+def test_serialize_many_stream_avro(ship_obj, ship_class):
+    ship_objs = [ship_obj, ship_obj]
+    data = io.BytesIO()
+    py_adapter.serialize_many_to_stream(ship_objs, data, format="Avro")
+    objs_out = list(py_adapter.deserialize_many_from_stream(data, ship_class, format="Avro"))
+    assert objs_out == ship_objs
